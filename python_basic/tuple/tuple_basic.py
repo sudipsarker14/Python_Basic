eb44a1e4cabaa0622a9798823be09ba21bbfcfb5
sudipsarker14 <sudipsarker2016@gmail.com>
2024-03-12 18:14:43 +0600

    dictionary

---
+++ python_basic/tuple/tuple_basic.py
@@ -0,0 +1,9 @@
+t = (20,30,40)
+
+l = len(t)
+
+for a in range(l):
+    print(t[a])
+
+for a in t:
+    print(a)
---
+++ python_basic/tuple/tuple_function.py
@@ -0,0 +1,19 @@
+t = (10,20,30,40,50)
+
+m = min(t)
+print(m)
+h = max(t)
+print(h)
+
+c = t.count(20)
+print(c)
+
+i = t.index(40)
+print(i)
+
+s = sum(t)
+print(s)
+
+
+e = sum(t, 10)
+print(e)
